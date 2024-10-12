4cb7876467c7a7a42a179d207d4a4f1306b0b653
MattPlayGamez <62027538+MattPlayGamez@users.noreply.github.com>
2024-10-12 09:22:12 +0200

    Added A LOT of documentation (JSDOC) which should make it more clear for everyone

--- mongodb.js
+++ mongodb.js
@@ -8,19 +8,39 @@ const Crypto = require('node:crypto')
 // Creëer het gebruikersmodel
 
 class Authenticator {
+    /**
+     * Constructor for the Authenticator class
+     * @param {string} QR_LABEL - label for the QR code
+     * @param {number} salt - salt for hashing passwords
+     * @param {string} JWT_SECRET_KEY - secret key for signing JWTs
+     * @param {object} JWT_OPTIONS - options for JWTs such as expiresIn
+     * @param {number} maxLoginAttempts - maximum number of login attempts
+     * @param {string} MONGODB_CONNECTION_STRING - connection string for MongoDB
+     * @param {mongoose.Schema} userSchema - schema for the User model
+     */
     constructor(QR_LABEL, salt, JWT_SECRET_KEY, JWT_OPTIONS, maxLoginAttempts, MONGODB_CONNECTION_STRING, userSchema) {
         this.QR_LABEL = QR_LABEL;
         this.salt = salt;
         this.JWT_SECRET_KEY = JWT_SECRET_KEY;
         this.JWT_OPTIONS = JWT_OPTIONS;
         this.maxLoginAttempts = maxLoginAttempts;
-
-        // Verbind met MongoDB
         mongoose.connect(MONGODB_CONNECTION_STRING);
-
         this.User = mongoose.model('User', userSchema)
+        this.OTP_ENCODING = 'base32'
+        this.lockedText = "User is locked"
+        this.OTP_WINDOW = 1 // How many OTP codes can be used before and after the current one (usefull for slower people, recommended 1)
+        this.INVALID_2FA_CODE_TEXT = "Invalid 2FA code"
+        this.REMOVED_USER_TEXT = "User has been removed" 
+        this.USER_ALREADY_EXISTS_TEXT = "User already exists"
+        this.ALLOW_DB_DUMP = false // Allowing DB Dumping is disabled by default can be enabled by setting ALLOW_DB_DUMP to true after initializing your class
     }
 
+    /**
+     * Registers a new user
+     * @param {object} userObject - object with required keys: email, password, wants2FA, you can add custom keys too
+     * @returns {object} - registered user object, or "Gebruiker bestaat al" if user already exists
+     * @throws {Error} - any other error
+     */
     async register(userObject) {
         try {
             const hash = await bcrypt.hash(userObject.password, this.salt);
@@ -35,7 +55,7 @@ class Authenticator {
                 const otpauth_url = speakeasy.otpauthURL({
                     secret: secret.base32,
                     label: this.QR_LABEL,
-                    encoding: 'base32'
+                    encoding: this.OTP_ENCODING
                 });
                 newUser.secret2FA = secret.base32;
                 await newUser.save();
@@ -51,19 +71,27 @@ class Authenticator {
 
         } catch (err) {
             if (err.code === 11000) {
-                return "Gebruiker bestaat al";
+                return this.USER_ALREADY_EXISTS_TEXT;
             }
             console.log(err);
             throw err;
         }
     }
 
+    /**
+     * Logs in a user
+     * @param {string} email - email address of the user
+     * @param {string} password - password of the user
+     * @param {number} twoFactorCode - 2FA code if user has 2FA enabled
+     * @returns {object} - logged in user object with JWT or qrCode if user has 2FA enabled
+     * @throws {Error} - any other error
+     */
     async login(email, password, twoFactorCode) {
         const user = await this.User.findOne({ email });
         if (!user) return null;
 
         try {
-            if (user.locked) return "User is locked"
+            if (user.locked) return this.lockedText
             const result = await bcrypt.compare(password, user.password);
             if (!result) {
 
@@ -83,7 +111,7 @@ class Authenticator {
                         const otpauth_url = speakeasy.otpauthURL({
                             secret: user.secret2FA,
                             label: this.QR_LABEL,
-                            encoding: 'base32'
+                            encoding: this.OTP_ENCODING
                         });
                         const qrCode = await QRCode.toDataURL(otpauth_url);
                         return { qrCode };
@@ -91,11 +119,11 @@ class Authenticator {
 
                     const verified = speakeasy.totp.verify({
                         secret: user.secret2FA,
-                        encoding: 'base32',
+                        encoding:  this.OTP_ENCODING,
                         token: twoFactorCode,
-                        window: 2 // Sta 2 tijdstippen toe voor en na de huidige tijd
+                        window: this.OTP_WINDOW
                     });
-                    if (!verified) return "Invalid 2FA code";
+                    if (!verified) return this.INVALID_2FA_CODE_TEXT;
 
                 }
                 const jwt_token = jwt.sign({ id: user._id, version: user.jwt_version }, this.JWT_SECRET_KEY, this.JWT_OPTIONS);
@@ -108,6 +136,15 @@ class Authenticator {
             throw err;
         }
     }
+    /**
+     * Generates a one time password and stores it in the user record. This is used
+     * for passwordless login, where the user will receive this code and can login
+     * with it.
+     * @param {string} email - the email of the user
+     * @returns {object} - object with a single key: emailCode, the code that the user
+     *                    needs to enter to login
+     * @throws {Error} - any error that occurs during the process
+     */
     async registerEmailSignin(email) {
         let emailCode = Crypto.randomUUID()
         try {
@@ -118,31 +155,44 @@ class Authenticator {
             console.error(error)
         }
     }
+    /**
+     * Verifies a emailCode and returns a valid JWT token for the user
+     * @param {string} emailCode - the emailCode to verify
+     * @returns {object} - an object with the user info and a valid JWT token
+     * @throws {Error} - any error that occurs during the process
+     */
     async verifyEmailSignin(emailCode) {
-        // Check if emailCode is valid and a string
         if (!emailCode || typeof emailCode !== 'string') return null;
 
-        // Query the user based on the emailCode
         const user = await this.User.findOne({ emailCode });
         if (!user) return null;
 
-        // Reset emailCode in the user record after successful verification
         await this.User.findOneAndUpdate({ emailCode }, { emailCode: "" });
 
-        // Generate a new JWT token for the user
         const jwt_token = jwt.sign(
             { id: user._id, version: user.jwt_version },
             this.JWT_SECRET_KEY,
             this.JWT_OPTIONS
         );
 
-        // Return user info and the new JWT token
-        return { ...user.toObject(), jwt_token };  // Use .toObject() to avoid returning Mongoose Document
+        return { ...user.toObject(), jwt_token };
     }
+    /**
+     * Retrieves user information based on the user ID
+     * @param {string} userId - the user ID to retrieve information
+     * @returns {object} - an object with the user information
+     * @throws {Error} - any error that occurs during the process
+     */
     async getInfoFromUser(userId) {
         return await this.User.findOne({ _id: userId });
     }
 
+    /**
+     * Verifies a JWT token and returns the user information if the token is valid
+     * @param {string} token - the JWT token to verify
+     * @returns {object} - the user information if the token is valid, otherwise false
+     * @throws {Error} - any error that occurs during the process
+     */
     async verifyToken(token) {
         
         try {
@@ -150,8 +200,6 @@ class Authenticator {
             if (jwt.verify(token, this.JWT_SECRET_KEY, this.JWT_OPTIONS)) {
                 let jwt_token = jwt.decode(token);
                 let user = await this.getInfoFromUser(jwt_token.id)
-                console.log("user")
-                console.log(user)
                 return (user.jwt_version === jwt_token.version) ? this.getInfoFromUser(jwt_token._id) : false;
             }
         } catch (error) {
@@ -159,38 +207,83 @@ class Authenticator {
 
         }
     }
+    /**
+     * Verifies a 2FA code for a user
+     * @param {string} userId - the user ID to verify the 2FA code for
+     * @param {string} twofactorcode - the 2FA code to verify
+     * @returns {boolean} - true if the code is valid, false otherwise
+     */
     async verify2FA(userId, twofactorcode) {
         let user = await this.User.findOne({ _id: userId })
         if (!user) return null
         const verified = speakeasy.totp.verify({
             secret: user.secret2FA,
-            encoding: 'base32',
+            encoding: this.OTP_ENCODING,
             token: twofactorcode,
-            window: 2 // Sta 2 tijdstippen toe voor en na de huidige tijd
+            window: this.OTP_WINDOW
         });
         return verified;
 
     }
+    /**
+     * Resets the password of a user
+     * @param {string} userId - the user ID to reset the password for
+     * @param {string} newPassword - the new password to set
+     * @returns {object} - the user object after the password has been reset
+     * @throws {Error} - any error that occurs during the process
+     */
     async resetPassword(userId, newPassword) {
         this.revokeUserTokens(userId)
         const hash = await bcrypt.hash(newPassword, this.salt);
         return await this.User.findOneAndUpdate({ _id: userId }, { password: hash }, { new: true })
 
     }
+    /**
+     * Changes the number of login attempts for a user
+     * @param {string} userId - the user ID to change the login attempts for
+     * @param {number} attempts - the new number of login attempts
+     * @returns {object} - the user object after the login attempts have been changed
+     * @throws {Error} - any error that occurs during the process
+     */
     async changeLoginAttempts(userId, attempts) {
         return await this.User.findOneAndUpdate({ _id: userId }, { loginAttempts: attempts }, { new: true });
+
     }
+    /**
+     * Locks a user from logging in
+     * @param {string} userId - the user ID to lock
+     * @returns {object} - the user object after the user has been locked
+     * @throws {Error} - any error that occurs during the process
+     */
     async lockUser(userId) {
         return await this.User.findOneAndUpdate({ _id: userId }, { locked: true }, { new: true });
     }
+    /**
+     * Unlocks a user from logging in
+     * @param {string} userId - the user ID to unlock
+     * @returns {object} - the user object after the user has been unlocked
+     * @throws {Error} - any error that occurs during the process
+     */
     async unlockUser(userId) {
         return await this.User.findOneAndUpdate({ _id: userId }, { locked: false }, { new: true });
     }
 
+    /**
+     * Revokes all user tokens for a user
+     * @param {string} userId - the user ID to revoke all tokens for
+     * @returns {object} - the user object after the tokens have been revoked
+     * @throws {Error} - any error that occurs during the process
+     */
     async revokeUserTokens(userId) {
         let newVersion = (await this.User.findOne({ _id: userId })).jwt_version + 1
         return await this.User.findOneAndUpdate({ _id: userId }, { jwt_version: newVersion }, { new: false });
     }
+    /**
+     * Removes 2FA for a user
+     * @param {string} userId - the user ID to remove 2FA for
+     * @returns {object} - the user object after 2FA has been removed
+     * @throws {Error} - any error that occurs during the process
+     */
     async remove2FA(userId) {
         return await this.User.findOneAndUpdate(
             { _id: userId },
@@ -198,6 +291,12 @@ class Authenticator {
             { new: true }
         );
     }
+    /**
+     * Adds 2FA for a user
+     * @param {string} userId - the user ID to add 2FA for
+     * @returns {object} - the user object after 2FA has been added
+     * @throws {Error} - any error that occurs during the process
+     */
     async add2FA(userId) {
         const user = await this.User.findOne({ _id: userId });
         if (!user) return null;
@@ -207,7 +306,7 @@ class Authenticator {
 
             secret: secret.base32,
             label: this.QR_LABEL,
-            encoding: 'base32'
+            encoding: this.OTP_ENCODING
         });
         const qrCode = await QRCode.toDataURL(otpauth_url);
 
@@ -217,15 +316,27 @@ class Authenticator {
             { new: true }
         );
     }
+    /**
+     * Removes a user from the database
+     * @param {string} userId - the user ID to remove
+     * @returns {string} - "User has been removed" if successful, otherwise an error message
+     * @throws {Error} - any error that occurs during the process
+     */
     async removeUser(userId) {
         try {
             await this.User.findOneAndDelete({ _id: userId });
-            return "User has been removed"
+            return this.REMOVED_USER_TEXT
         } catch (error) {
             return `User with ID ${userId} couldn't be removed`
         }
     }
+    /**
+     * Retrieves all users from the database
+     * @returns {object[]} - an array of user objects
+     * @throws {Error} - any error that occurs during the process
+     */
     async dumpDB() {
+        if (this.ALLOW_DB_DUMP === false) return "DB dumping is disabled"
         return await this.User.find()
     }
 
